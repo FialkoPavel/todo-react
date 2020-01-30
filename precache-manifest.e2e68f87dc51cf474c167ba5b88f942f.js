6196a83f1d69a6a3d6d5d36a6e8eae1a54c31a46
Pavel <fialkopavel@gmail.com>
2020-01-30 15:03:14 +0200

    Updates

---
+++ precache-manifest.e2e68f87dc51cf474c167ba5b88f942f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6ead9032166c8bb3d8e519dc55c36ee",
+    "url": "/todo-react/index.html"
+  },
+  {
+    "revision": "c3e59820c4a1367e4806",
+    "url": "/todo-react/static/css/main.32593838.chunk.css"
+  },
+  {
+    "revision": "0218f4b83bf6890a6222",
+    "url": "/todo-react/static/js/2.efb50979.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/todo-react/static/js/2.efb50979.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c3e59820c4a1367e4806",
+    "url": "/todo-react/static/js/main.df629a31.chunk.js"
+  },
+  {
+    "revision": "2e0be414792027b9e872",
+    "url": "/todo-react/static/js/runtime-main.86f98c05.js"
+  }
+]);
